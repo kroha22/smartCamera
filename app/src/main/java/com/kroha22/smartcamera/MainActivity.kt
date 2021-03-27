79e5fc65732cef663afffbb8f29944cbaedb91f3
olga_kroha <olga.kroha22@gmail.com>
2021-03-27 15:37:04 +0700

    build

---
+++ app/src/main/java/com/kroha22/smartcamera/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kroha22.smartcamera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
